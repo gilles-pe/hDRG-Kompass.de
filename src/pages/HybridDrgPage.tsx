247d0d83fa572a0349023e6244c7e98f54e0131b
gilles-pe <gillespenning@gmail.com>
2026-02-05 10:43:53 +0100

    Fix Hybrid-DRG image URL encoding

--- src/pages/HybridDrgPage.tsx
+++ src/pages/HybridDrgPage.tsx
@@ -1,6 +1,6 @@
 function HybridDrgPage() {
   const baseUrl = import.meta.env.BASE_URL
-  const principleImage = `${baseUrl}hDRG Prinzip.jpg`
+  const principleImage = `${baseUrl}${encodeURIComponent('hDRG Prinzip.jpg')}`
 
   return (
     <div className="page">
